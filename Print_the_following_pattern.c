d21b7e80cbd57ff96b48211f1e8e7dc612dd411b
21A91A1212 <98648618+21A91A1212@users.noreply.github.com>
2022-05-28 20:28:23 +0530

    #include<stdio.h> int main() {     int rows,i,j;     scanf("%d",&rows);     for(i=1;i<=rows;i++)     {         for(j=i;j<=rows;j++)         {             printf("%d ",j);         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int rows,i,j;
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+        for(j=i;j<=rows;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+}
