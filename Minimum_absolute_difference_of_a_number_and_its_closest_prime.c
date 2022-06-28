dd536dd694c9cabd8855ce79ee582d80d36a5326
21A91A1212 <98648618+21A91A1212@users.noreply.github.com>
2022-06-28 22:50:25 +0530

    #include<stdio.h> int main() {     int n,i,j,k,np,sp,c=0,d,e;     scanf("%d",&n);     for(j=n;j>=n-10;j--)     {         c=0;         for(k=1;k<=j;k++)         {             if(j%k==0)         {                 c++;             }         }         if(c==2)         {             np=j;             d=n-np;             break;         }     }     for(j=n;j<=n+10;j++)     {     	c=0;         for(k=1;k<=j;k++)         {             if(j%k==0)             {                 c++;             }         }         if(c==2)         {             sp=j;             e=sp-n;             break;         }     }     if(d<e || d==e)     {         printf("%d",d);     }     else     {         printf("%d",e);     } }

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -1,46 +1,48 @@
 #include<stdio.h>
-#include<math.h>
-int isprime(int num)
-{
-    int i,fc=0;
-	for(i=1;i<=num;i++)
-	{
-		if(num%i==0)
-		{
-			fc++;	
-		}	
-	}
-	if(fc==2)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
 int main()
 {
-	int num,pp,np,avg,res,r1,r2;
-	float sq;
-	scanf("%d",&num);
-	if(isprime(num))
-	{
-	    printf("0");
-	}
-	else
-	{
-    	for(pp=num-1;!isprime(pp);pp--);
-    	for(np=num+1;!isprime(np);np++);
-    	r1=np-num;
-    	r2=num-pp;
-    	if(r1>r2)
-    	{
-    	    printf("%d",r2);
-    	}
-    	else
-    	{
-    	    printf("%d",r1);
-    	}
-	}
+    int n,i,j,k,np,sp,c=0,d,e;
+    scanf("%d",&n);
+    for(j=n;j>=n-10;j--)
+    {
+        c=0;
+        for(k=1;k<=j;k++)
+        {
+            if(j%k==0)
+        {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            np=j;
+            d=n-np;
+            break;
+        }
+    }
+    for(j=n;j<=n+10;j++)
+    {
+    	c=0;
+        for(k=1;k<=j;k++)
+        {
+            if(j%k==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            sp=j;
+            e=sp-n;
+            break;
+        }
+    }
+    if(d<e || d==e)
+    {
+        printf("%d",d);
+    }
+    else
+    {
+        printf("%d",e);
+    }
 }
